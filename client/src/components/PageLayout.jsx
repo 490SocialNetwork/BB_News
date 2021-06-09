9c089c61ff9b04d9184785df81f5e5e8ce0c4dc0
unknown <ahmedz123645@gmail.com>
2021-06-08 22:02:28 -0400

    new Home Page

--- client/src/components/PageLayout.jsx
+++ client/src/components/PageLayout.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-import { Navbar, Container, Nav, Button } from "react-bootstrap";
-const PageLayout = ({ admin }) => {
+import { Navbar, Container, Form, FormControl, Button } from "react-bootstrap";
+const PageLayout = (props) => {
   return (
     <Layout>
       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand>BasketBall News</Navbar.Brand>
-          <h4 className="me-auto ">{admin ? "Admin" : "User"}</h4>
+          <h4 className="me-auto ">{props.admin ? "Admin" : "User"}</h4>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search Users"
+              className="mr-2"
+              aria-label="Search"
+            />
+          </Form>
         </Container>
       </Navbar>
+      {props.children}
     </Layout>
   );
 };
